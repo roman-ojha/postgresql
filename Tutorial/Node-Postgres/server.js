8dff64370487ac56cc98852fdbd787e37933fa9e
Roman-Ojha <razzroman98@gmail.com>
2022-08-29 23:02:54 +0545

    feat: querying on postgres

--- Tutorial/Node-Postgres/server.js
+++ Tutorial/Node-Postgres/server.js
@@ -1,9 +1,21 @@
 import express from "express";
 import client from "./config/db.js";
+
 // https://node-postgres.com/features/connecting
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+client.query(
+  "SELECT * FROM person WHERE country_of_birth ILIKE 'nepal'",
+  (err, res) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(res);
+    }
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Running on ${PORT}`);
