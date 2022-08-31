2ee27bde4c77db8458826405fa0af0c5a573f91f
Roman-Ojha <razzroman98@gmail.com>
2022-08-31 10:02:34 +0545

    docs: added relationship between table

--- Tutorial/00_Main_Tutorial/37_Adding_Relationships_Between_Tables/person-car.sql
+++ Tutorial/00_Main_Tutorial/37_Adding_Relationships_Between_Tables/person-car.sql
@@ -1,12 +1,4 @@
-create table person (
-	id BIGSERIAL NOT NULL PRIMARY KEY,
-	first_name VARCHAR(50) NOT NULL,
-	last_name VARCHAR(50) NOT NULL,
-	email VARCHAR(150),
-	gender VARCHAR(10) NOT NULL,
-	date_of_birth DATE NOT NULL,
-	country_of_birth VARCHAR(50)
-);
+-- so what we are tyring to achive is 'a person can have one car' & 'car can only belong to one person' 
 
 create table car (
 	id BIGSERIAL NOT NULL PRIMARY KEY,
@@ -15,10 +7,28 @@ create table car (
 	price NUMERIC(50) NOT NULL
 );
 
+create table person (
+	id BIGSERIAL NOT NULL PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	email VARCHAR(150),
+	gender VARCHAR(10) NOT NULL,
+	date_of_birth DATE NOT NULL,
+	country_of_birth VARCHAR(50) NOT NULL,
+	-- let's add relation between car
+	car_id BIGINT REFERENCES car(id),
+	UNIQUE(car_id)
+	-- we can't add 'BIGSERIAL' here because it is a special datatype that is being managed by sequence
+	-- rather we have to use 'BIGINT' and that are same in terms of actual size
+	-- and also we will not add 'NOT NULL' because person may no have a car
+	-- no to reference to another table column we have to use 'REFERENCES' keyword 
+	-- and also we have says that 'car can only be own by one person' it means that we can add a unique constraint
+);
+
+
 insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Coraline', 'Bellward', 'cbellward0@newsvine.com', 'Female', '2022-01-08', 'Sweden');
 insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Hayley', 'Volker', null, 'Female', '2022-07-05', 'Maldives');
 insert into person (first_name, last_name, email, gender, date_of_birth, country_of_birth) values ('Mona', 'Targett', null, 'Female', '2021-09-14', 'Philippines');
 
-
-insert into car (id, make, model, price) values (1, 'Cadillac', 'Escalade ESV', '76446.18');
-insert into car (id, make, model, price) values (2, 'Nissan', 'Frontier', '21524.06');
+insert into car (make, model, price) values ('Cadillac', 'Escalade ESV', '76446.18');
+insert into car (make, model, price) values ('Nissan', 'Frontier', '21524.06');
