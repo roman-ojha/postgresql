27434d7920770b07ec2590cd2cfb03cf8525f14b
Roman-Ojha <razzroman98@gmail.com>
2022-09-02 16:55:48 +0545

    docs: small note for node postgres

--- Tutorial/01_Node_Postgres/server.js
+++ Tutorial/01_Node_Postgres/server.js
@@ -1,8 +1,6 @@
 import express from "express";
 import client from "./config/db.js";
 
-// https://node-postgres.com/features/connecting
-
 const app = express();
 const PORT = process.env.PORT || 8080;
 
